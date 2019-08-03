322d5d0680a9b32f0dfdd4f66f69e7f7c9977ffc
Xandronus <xandronus@gmail.com>
2019-08-03 14:19:28 -0600

    Add some updates, including: Add users wallet address Pull messages from server Formatting

--- index.js
+++ index.js
@@ -7,19 +7,31 @@ const querystring = require('querystring')
 //const config = require('./config.json')
 const mongoose = require('mongoose')
 
+// ---------------------------------------------
+// Config
+// ---------------------------------------------
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+// ---------------------------------------------
+// Database
+// ---------------------------------------------
+
 var userSchema = null 
 var messageSchema = null
+var appSchema = null
+var visitSchema = null
 
 const database = {   
   getModels: async function() {    
     var User = await mongoose.model('User', userSchema)
     var Message = await mongoose.model('Message', messageSchema)
+    var App = await mongoose.model('App', appSchema)
+    var Visit = await mongoose.model('Visit', visitSchema)
   
-    return {'User': User, 'Message': Message}
+    return {'User': User, 'Message': Message, 'App': App, 'Visit': Visit}
   },
 
   connect: async function(onConnected) {
@@ -52,8 +64,29 @@ const database = {
         nick: String
       })
     }
+
+    if (!appSchema) {
+      appSchema = new mongoose.Schema({
+        _id: mongoose.Schema.Types.ObjectId,
+        name: String
+      })
+    }
+
+    if (!visitSchema) {
+      visitSchema = new mongoose.Schema({
+        _id: mongoose.Schema.Types.ObjectId,
+        nickid: mongoose.Schema.Types.ObjectId,
+        appid: mongoose.Schema.Types.ObjectId,
+        parcel: {x:Number, y:Number},
+        visits: Number
+      })
+    }
   }
 }
+
+// ---------------------------------------------
+// Requests
+// ---------------------------------------------
 
 async function requestTest(option, callback) {
   callback(false, {'success': true, 'message': option})
@@ -80,7 +113,7 @@ async function requestCreateUser(user, callback) {
         if (err) 
           callback({'success': false, message: `Cannot create user ${user.nick}.`}, false)
         else {
-          var returnVal = {id: newId}
+          var returnVal = {_id: newId, nick: user.nick}
           callback(false, {'success': true, 'message': returnVal})
         }
       });
@@ -130,7 +163,11 @@ async function requestGetUser(user, callback) {
     if (model) {
       model.User.find({ nick: user.nick }, function (err, users) {
         if (err) {
-          callback({'success': false, message: `No user found by nick ${user.nick}.`}, false)
+          if (user.create) {
+            requestCreateUser(user, callback)
+          } else {          
+            callback({'success': false, message: `No user found by nick ${user.nick}.`}, false)
+          }
         }
         else {
           var returnVal = users
@@ -163,6 +200,10 @@ async function requestGetMessages(options, callback) {
     }
   })
 }
+
+// ---------------------------------------------
+// Command Dispatcher
+// ---------------------------------------------
 
 var commandDispatcher = {
     connectdb: function(options) {
@@ -232,6 +273,10 @@ var commandDispatcher = {
       })
     }
 }
+
+// ---------------------------------------------
+// Entry
+// ---------------------------------------------
 
 module.exports = async (req, res) => {
   if (req.method === 'OPTIONS') {
